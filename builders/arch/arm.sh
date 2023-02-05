f5e0abf0c8b4caca856a39d3af46bc48745bf6ac
V8 macOS Builder <v8.macos.builder@localhost>
2023-02-05 16:10:50 +0000

    done

--- builders/arch/arm.sh
+++ builders/arch/arm.sh
@@ -1,5 +1,5 @@
 function build_dir {
-  echo -n ~/v8/v8/out.gn/$(arch_label).$(scheme_label)/clang_x64_v8_arm
+  echo -n ~/v8/v8/out.gn/$(arch_label).$(scheme_label)/clang_x86_v8_arm
 }
 
 function arch_label {
