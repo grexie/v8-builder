55ea17ecef8af3f8b5f1a92afdc7667a1c248bf9
Tim Behrsin <timothy@behrsin.org>
2023-06-10 16:36:58 +0100

    Create ios.sh

---
+++ builders/platform/ios.sh
@@ -0,0 +1,37 @@
+
+function platform_label {
+  echo -n mac
+}
+
+function copy_assets {
+  find $(build_dir) -type f -name "*.dylib"
+  find $(build_dir) -type f -name "*.a"
+  find $(build_dir) -type f -name "*.dat" | head -n1
+  echo $(build_dir)/bytecode_builtins_list_generator
+  echo $(build_dir)/gen-regexp-special-case
+  echo $(build_dir)/mksnapshot
+}
+
+function platform_setup {
+  brew install ninja
+}
+
+function platform_requirements {
+  echo -n
+}
+
+function platform_additions {
+  echo -n
+}
+
+function platform_patches {
+  echo -n
+}
+
+function platform_build {
+  echo -n
+}
+
+function platform_ninja {
+  echo -n
+}
